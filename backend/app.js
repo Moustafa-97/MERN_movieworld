e0683bd263885d23143d5e941b3a0306b3c31bb3
Moustafa <moustafaadel097@gmail.com>
2024-02-23 11:06:25 +0200

    mern

--- backend/app.js
+++ backend/app.js
@@ -28,8 +28,8 @@ const {
 } = require("./controllers/UserControl");
 
 if (process.env.NODE_ENV === "production") {
-  const __dirname = path.resolve("../");
-  app.use(express.static(path.join(__dirname + "frontendmovieclient/build")));
+  const __dirname = path.resolve("/");
+  app.use(express.static(path.join(__dirname + "/frontendmovieclient/build")));
   app.get("*", (req, res, next) => {
     res.sendFile(
       path.join(__dirname, "frontendmovieclient", "build", "index.html")
